1fce291e8537633f03ab23d62d70c0fa40253bcb
YusufSizmaz <yusuf.sizmaz11@gmail..com>
2025-02-18 13:59:50 +0300

    login page final

--- src/components/input/Passwordinput.jsx
+++ src/components/input/Passwordinput.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+
 const PasswordInput = ({ value, onChange, placeholder }) => {
   const [isShowPassword, setIsShowPassword] = useState(false);
 
@@ -9,14 +11,28 @@ const PasswordInput = ({ value, onChange, placeholder }) => {
 
   return (
     <>
-      <div className="flex items-center bg-transparent border-1[1.5px] px-5 rounded mb-3">
+      <div className="flex items-center bg-transparent border border-gray-300 px-5 rounded mb-3">
         <input
           value={value}
           onChange={onChange}
           type={isShowPassword ? "text" : "password"}
           placeholder={placeholder || "Password"}
-          className="input-box w-full text-md bg-transparent  px-5 py-3 rounded mb-4 bg-primary  p-2  my-1  border-2"
+          className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
         />
+
+        {isShowPassword ? (
+          <FaRegEye
+            size={22}
+            className="text-blue-500 cursor-pointer"
+            onClick={() => toggleShadowPassword()}
+          />
+        ) : (
+          <FaRegEyeSlash
+            size={22}
+            className="text-slate-400 cursor-pointer"
+            onClick={() => toggleShadowPassword()}
+          />
+        )}
       </div>
     </>
   );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,27 +8,30 @@ const Login = () => {
     <>
       <Navbar />
       <div className="flex items-center justify-center mt-28">
-        <div className="w-96 border rounded bg-white px-7 py-10">
+        <div className="w-96 border border-gray-300 rounded bg-white px-7 py-10">
           <form onSubmit={() => {}}>
             <h4 className="text-2xl mb-7">Login</h4>
 
             <input
               type="text"
               placeholder="Email"
-              className="input-box w-full text-md bg-transparent  px-5 py-3 rounded mb-4 bg-primary  p-2  my-1  border-2"
+              className="w-full text-sm bg-transparent px-5 py-3 rounded mb-5 border border-gray-300 focus:border-gray-400 outline-none"
             />
 
             <PasswordInput />
 
             <button
               type="submit"
-              className="w-full text-md  border-[1.5px] px-5 py-3 rounded mb-4 outline-none bg-blue-500 text-white  hover:bg-blue-600"
+              className="w-full text-md border border-gray-300 px-5 py-3 rounded mb-4 outline-none bg-blue-500 text-white hover:bg-blue-600"
             >
               Login
             </button>
             <p className="text-sm text-center mt-4">
-              Not registered yet? {""}
-              <Link to="/signUp" className="font-medium text-primary underline">
+              Not registered yet?{" "}
+              <Link
+                to="/signUp"
+                className="font-medium text-blue-500 underline hover:text-blue-800"
+              >
                 Create an Account
               </Link>
             </p>
